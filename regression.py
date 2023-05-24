76aded7602a31f6ad618a960fe8f88bd1c2f3d82
Luca Berbenni <lucaberbenni@mbp-di-luca.localdomain>
2023-05-24 07:32:58 +0200

    train test split solved

--- regression.py
+++ regression.py
@@ -1,10 +1,12 @@
 import pandas as pd
 
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
 from sklearn.pipeline import make_pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV, cross_val_score
 
 '''
 import train and test df
@@ -36,7 +38,9 @@ define X, y
 '''
 X = df.drop(['count', 'registered', 'casual'], axis=1)
 y = df['count']
-
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                    test_size = 0.2, 
+                                                    random_state = 42)
 '''
 define the transformation pipeline and fit the model
 '''
@@ -73,18 +77,19 @@ transform = make_pipeline(
     )
 )
 
-m = make_pipeline(
-    transform, 
-    RandomForestRegressor()
-)
-m.fit(X, y)
+X_train_t = transform.fit_transform(X_train)
+X_test_t = transform.transform(X_test)
+m = RandomForestRegressor()
+
+m.fit(X_train_t, y_train)
+
 
 '''
 define hyperparameters to optimize and cross validate
 '''
 parameters = {
-    'randomforestregressor__n_estimators':[100, 120], 
-    'randomforestregressor__max_depth':[8, 16]
+    'n_estimators':[100, 120], 
+    'max_depth':[8, 16]
 }
 grid_cv = GridSearchCV(
     estimator=m, 
@@ -92,14 +97,19 @@ grid_cv = GridSearchCV(
     cv = 5, 
     scoring='r2'
 )
-grid_cv.fit(X, y)
+grid_cv.fit(X_train_t, y_train)
 
 '''
 define best estimator, fit that model and make prediction
 '''
 m_best = grid_cv.best_estimator_
-m_best.fit(X, y)
-y_pred = m_best.predict(kaagle)
+m_best.fit(X_train_t, y_train)
+m_pred = m_best.predict(X_test_t)
+
+print(r2_score(y_test, m_pred))
+
+kaagle_t = transform.transform(kaagle)
+y_pred = m_best.predict(kaagle_t)
 
 '''
 create a df for kaagle submission and save it as .csv file
